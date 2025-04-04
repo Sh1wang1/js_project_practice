e60fffcc7c925560d8044c35b4b424234bb23479
Sh1wang1 <shiwangiverma2003@gmail.com>
2025-04-04 17:48:49 +0530

    Updated Rock-Paper-Scissors game with sound effects

--- game/script.js
+++ game/script.js
@@ -1,43 +1,56 @@
-
-let user = 0;
-let comp = 0;
+let user =  0;
+let comp =  0;
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#msg");
 const users = document.querySelector("#user");
 const comps = document.querySelector("#comp");
 
+users.innerText = user;
+comps.innerText = comp;
+
+const clickSound = new Audio("sound/click.mp3");
+const winSound = new Audio("sound/win.mp3");
+const loseSound = new Audio("sound/loose.mp3");
+
 const gameChoice = () => {
     const options = ["rock", "paper", "scissor"];
     const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
-}
+};
 
 const drawGame = () => {
-    console.log("Game was draw");
-    msg.innerText = "Game was draw, Play again";
+    msg.innerText = "Game was a draw! Play again";
     msg.style.backgroundColor = "black";
-}
+    msg.classList.add("fade-in");
+};
 
 const showWinner = (userWin, userChoice, compChoice) => {
     if (userWin) {
-        console.log("You Win");
         user++;
         users.innerText = user;
         msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
         msg.style.backgroundColor = "green";
+        winSound.play();
     } else {
-        console.log("You Lose");
         comp++;
         comps.innerText = comp;
-        msg.innerText = `You Lost!${compChoice} beats ${userChoice}`;
+        msg.innerText = `You Lost! ${compChoice} beats ${userChoice}`;
         msg.style.backgroundColor = "red";
+        loseSound.play();
     }
-}
+
+
+};
 
 const playGame = (userChoice) => {
-    console.log(userChoice);
+    clickSound.play();
     const compChoice = gameChoice();
-    console.log(compChoice);
+    
+    msg.classList.add("shake");
+    setTimeout(() => {
+        msg.classList.remove("shake");
+    }, 500);
+    
     if (userChoice === compChoice) {
         drawGame();
     } else {
@@ -51,7 +64,7 @@ const playGame = (userChoice) => {
         }
         showWinner(userWin, userChoice, compChoice);
     }
-}
+};
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
